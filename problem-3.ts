731ada9df05d5bbd472e35834d2f8dca4da07953
rashidsarker <rashidrock558@gmail.com>
2024-11-06 19:16:07 +0600

    task 3 done return the number of times the word appears in the sentence

---
+++ problem-3.ts
@@ -0,0 +1,18 @@
+{
+  const countWordOccurrences = (sentence: string, word: string): number => {
+    // const result: number = sentence.search(word);
+    // console.log(result);
+    // return result;
+    let sentenArr: string[] = sentence.split(" ");
+    let findWord: string[] = sentenArr.filter((item: string) => {
+      return item.toLowerCase().includes(word.toLowerCase());
+    });
+    // console.log(findWord);
+
+    // console.log(sentenArr);
+    console.log(findWord.length);
+    return findWord.length;
+  };
+
+  countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript");
+}
